8b638cdebe587bec89b22762e18e1ab56db0da39
iosliutingxin <17787148484@qq.com>
2018-05-25 21:45:25 +0800

    初始化项目

---
+++ GCD/GCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD
+//
+//  Created by 李孔文 on 2018/5/25.
+//  Copyright © 2018年 Sunning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD/GCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD
+//
+//  Created by 李孔文 on 2018/5/25.
+//  Copyright © 2018年 Sunning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
